825474f0120e94888a9c9009df16f908905da469
JuanSaq <jsaquino04@gmail.com>
2025-10-12 16:51:41 -0400

    enabled cors, got data from backend

--- apps/web-start/src/routes/dashboard.tsx
+++ apps/web-start/src/routes/dashboard.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
 import "./dashboard.css"
+import { useQuery } from '@tanstack/react-query';
+import {Course} from '@repo/database'
+import { backendFetcher } from '../integrations/fetcher';
 
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
@@ -8,9 +11,18 @@ export const Route = createFileRoute('/dashboard')({
 
 // temp ids for now, will map later
 function RouteComponent() {
+  const { isPending, isError, data, error } = useQuery<Array<Course>>({
+    queryKey: ['courses'],
+    queryFn: backendFetcher("/courses"),
+  })
+
+  if (isPending) return <span>Is Loading...</span>;
+
+  if (isError) return <span>Error: {error.message}</span>
+
+  /*
   return (
-  
-    <main>
+      <main>
         <h1>Hello "/dashboard"!</h1>
         <div className="courseGrid">
             <Link to="/course/$courseId" params={{courseId: "1"}} className="courseButton"> test</Link>
@@ -21,4 +33,17 @@ function RouteComponent() {
         </div>
     </main>
   )
+    */
+
+  return (
+    <div className='courseGrid'>
+      {data.map((course) => (
+        <div key={course.id}>
+          <Link to="/course/$courseId" params={{courseId: course.id}} className='courseButton'>{course.title}</Link>
+        </div>
+      ))}
+    </div>
+
+  )
+  
 }
